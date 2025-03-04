4383f859d34445db6d6cd172f5452562bc9a41b6
Shashwat Raj <rajayshashwat@gmail.com>
2025-03-03 21:18:12 -0700

    added fnv-1a hash function

--- p1_starter/hash.cpp
+++ p1_starter/hash.cpp
@@ -1,8 +1,64 @@
 #include <iostream>
 #include <string>
+#include <functional>
+#include <cmath>
 #include "hash.h"
 
 using namespace std;
+
+int computeSlot(const std::string &s, int k) {
+    static std::hash<std::string> hasher;
+    return static_cast<int>(hasher(s) % k);
+    }
+
+HashTable::~HashTable() {}
+
+HashTable::HashTable(int k)
+    : table(k){}
+    
+void HashTable::insert(const std::string &token) {
+    if (table.empty()) return;  // if k==0
+    int slotIndex = computeSlot(token, getNumSlots());
+    table[slotIndex].push_back(token);
+}
+
+std::vector<std::string> HashTable::getSlot(int index) const {
+    if (index >= 0 && index < getNumSlots()) {
+        return table[index];
+    } else{
+        return {};
+    }
+}
+
+
+int HashTable::getSlotLength(int index) const {
+    if (index >= 0 && index < getNumSlots()) {
+        return static_cast<int>(table[index].size());
+    }
+    return 0;
+}
+
+int HashTable::getNumSlots() const {
+    return static_cast<int>(table.size());
+}
+
+double HashTable::getStandardDeviation() const {
+    int k = getNumSlots();
+    if (k == 0) return 0.0;
+    double mean = 0.0;
+    for (int i = 0; i < k; i++) {
+        mean += table[i].size();
+    }   
+    mean /= k;
+    double variance = 0.0;
+
+    
+    for (const auto &slot : table) {
+        double diff = slot.size() - mean;
+        variance += (diff * diff)/k;
+    }
+
+}
 
 int hash_function(string text) {
     // Implement your own hash function here
--- p1_starter/hash.h
+++ p1_starter/hash.h
@@ -5,4 +5,21 @@
 
 using namespace std;
 
-int hash_function(string text);
+int hashFunction(const std::string &s, int k);
+
+class HashTable {
+public:
+    HashTable(int k);
+    ~HashTable();
+    void insert(const std::string &s);
+    std::vector<std::string> getSlot(int index) const;
+    int getSlotLength(int i) const;
+    int getNumSlots() const;
+    double getStandardDeviation() const;
+    void printFirstFiveSlots() const;
+
+ private:
+    std::vector<std::vector<std::string>> table;
+};
+
+#endif
